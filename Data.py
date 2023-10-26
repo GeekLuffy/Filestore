c3dd355f0824d032387e5673ec1821740d456245
GeekLuffy <71143694+GeekLuffy@users.noreply.github.com>
2023-10-26 14:23:11 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,24 +6,24 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> ❏ Perintah untuk Pengguna BOT
- ├ /start - Mulai Bot
- ├ /about - Tentang Bot ini
- ├ /help - Bantuan Perintah Bot ini
- ├ /ping - Untuk mengecek bot hidup
- └ /uptime - Untuk melihat status bot 
+<b> ❏ Commands for BOT Users
+  ├ /start - Start Bot
+  ├ /about - About this Bot
+  ├ /help - Help this Bot Command
+  ├ /ping - To check live bots
+  └ /uptime - To view bot status
  
- ❏ Perintah Untuk Admin BOT
- ├ /logs - Untuk melihat logs bot
- ├ /setvar - Untuk mengatur var dengan command dibot
- ├ /delvar - Untuk menghapus var dengan command dibot
- ├ /getvar - Untuk melihat salah satu var dengan command dibot
- ├ /users - Untuk melihat statistik pengguna bot
- ├ /batch - Untuk membuat link lebih dari satu file
- ├ /speedtest - Untuk Mengetes kecepatan server bot
- └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
+  ❏ Commands For BOT Admin
+  ├ /logs - To view bot logs
+  ├ /setvar - To set the var with the dibot command
+  ├ /delvar - To delete var with dibot command
+  ├ /getvar - To view one of the vars with the dibot command
+  ├ /users - To view bot user statistics
+  ├ /batch - To link more than one file
+  ├ /speedtest - To test bot server speed
+  └ /broadcast - To send broadcast messages to bot users
 
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+👨‍💻 Develoved by </b><a href='https://t.me/GeekLuffy'>Luffy</a>
 """
 
     close = [
@@ -33,25 +33,25 @@ class Data:
     mbuttons = [
         [
             InlineKeyboardButton("ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
-            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+            InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
         ],
     ]
 
     buttons = [
         [
-            InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data="about"),
-            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+            InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="about"),
+            InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
         ],
     ]
 
     ABOUT = """
-<b>Tentang Bot ini:
+<b>About this Bot:
 
-@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+ @{} is a Telegram Bot for saving Posts or Files that can be Accessed via a Special Link.
 
- • Creator: @{}
- • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
- • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
+  • Creator: @{}
+  • Framework: Pyrogram
+  • Source Code: <a href='https://t.me/Monkey_d_luufy'>Filebot</a>
 
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+👨‍💻 Develoved by </b><a href='https://t.me/GeekLuffy'>Luffy</a>
 """
